bf360ed5e4e1ee3266eec38c616d4fc6c9a9fa51
James Balnaves <james@balnaves.com>
2020-05-22 15:17:38 -0700

    Update example package URLs to public ones.

--- Examples/SimpleDelegate/Package.swift
+++ Examples/SimpleDelegate/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SimpleDelegate",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../../"),
+        .package(url: "http://github.com/balnaves/SwiftySR700.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
